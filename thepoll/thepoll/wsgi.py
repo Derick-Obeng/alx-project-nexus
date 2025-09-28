8d18382dc2c418aa27d993969fa9fff9e05a8b20
Derrick Obeng <derickobeng023@gmail.com>
2025-09-28 15:16:50 +0000

    was reloading server

--- thepoll/thepoll/wsgi.py
+++ thepoll/thepoll/wsgi.py
@@ -7,12 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
 
+import os
 from django.core.wsgi import get_wsgi_application
 
-from thepoll import thepoll
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thepoll.thepoll.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thepoll.settings')
-    
 application = get_wsgi_application()
+
+
