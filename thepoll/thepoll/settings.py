32ac632d067928dda5146d9b45374936e37bc060
Derrick Obeng <derickobeng023@gmail.com>
2025-09-28 07:13:36 +0000

    commited from settings

--- thepoll/thepoll/settings.py
+++ thepoll/thepoll/settings.py
@@ -17,6 +17,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
