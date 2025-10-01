c07a0e7bfd8be0d40794c9e75a70436edde4ab40
Derrick Obeng <derickobeng023@gmail.com>
2025-10-01 09:48:22 +0000

    corrected url paths

--- thepoll/thepoll/urls.py
+++ thepoll/thepoll/urls.py
@@ -39,6 +39,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', include('poll.urls')),  # Root URL now points to poll app
     path('admin/', admin.site.urls),
     path('poll/', include('poll.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
